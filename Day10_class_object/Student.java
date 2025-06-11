f1fbf1f9cf450ac618663e1804425a511e02be3b
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:26:56 +0530

    Create Student.java

---
+++ Day10_class_object/Student.java
@@ -0,0 +1,16 @@
+package day10_class_object;
+
+public class Student {
+	// Class contains variable and methods
+	
+	// Variable
+	int sid;
+	String name;
+	char grade;
+	
+	void print()
+	{
+		System.out.println(sid+"  "+name+"  "+grade);
+	}
+	
+}
