56ed491b73263796ed0503f9138c60e4bdc62425
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:05:24 +0530

    Create WhileVSDoWHileloop.java

---
+++ Day5/WhileVSDoWHileloop.java
@@ -0,0 +1,29 @@
+package day5;
+
+public class WhileVSDoWHileloop {
+
+	public static void main(String[] args) {
+		
+		//while loop
+		//Here first condition will check and after that print the statement
+		/*int i=11;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+        */
+		
+		
+		//Do while loop.
+		// Here print statement will execute first and after that check the condition.
+		
+		int i=13;
+		do 
+		{
+		System.out.println(i);	
+		i++;
+		}while(i<=10);
+	}
+
+}
