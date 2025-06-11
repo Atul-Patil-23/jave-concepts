b3081020e0d3c0596304277de051bc6768f627b3
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:37:46 +0530

    Create FinalKeyword.java

---
+++ Day15/FinalKeyword.java
@@ -0,0 +1,18 @@
+package day15;
+
+class Test
+{
+	final int x=100;
+}
+
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		Test t=new Test();
+		 //t.x=200; - we can't reassign becaz x is final variable.
+		System.out.println(t.x);
+
+	}
+
+}
