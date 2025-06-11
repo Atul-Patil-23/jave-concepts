f5802e6deb1a96880f5fca8841face61f6735590
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:42:40 +0530

    Create CheckedExceptions.java

---
+++ Day18/CheckedExceptions.java
@@ -0,0 +1,24 @@
+package day18;
+
+public class CheckedExceptions {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Program started");
+		System.out.println("Program in progress");
+		
+		Thread.sleep(0);
+		
+		
+		/*try {
+		Thread.sleep(5000);
+		}
+		catch(Exception e)
+		{	
+		}
+		*/
+		System.out.println("Program ended");
+		System.out.println("Program is passed");
+		
+	}
+
+}
