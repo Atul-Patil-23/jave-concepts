fdc583804737b08e2a90dbe507a80ec946f4f886
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:22:08 +0530

    Create RemoveJunk.java

---
+++ Day9/RemoveJunk.java
@@ -0,0 +1,12 @@
+  package day9;
+
+public class RemoveJunk {
+
+	public static void main(String[] args) {
+		String s="$^& Testing )(*&^%java%^&and!@#$%seleni*&^%um";
+		s=s.replaceAll("[^a-zA-Z0-9]","");
+		System.out.println(s);
+
+	}
+
+}
