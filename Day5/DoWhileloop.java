25a27d85092d94a3eb3a9094d6f7ac2280a3eacd
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:01:55 +0530

    Create DoWhileloop.java

---
+++ Day5/DoWhileloop.java
@@ -0,0 +1,28 @@
+package day5;
+
+public class DoWhileloop {
+
+	public static void main(String[] args) {
+		
+		// 1 to 10 number 
+		/*int i=1;
+		do 
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+		*/
+		
+		
+		//10, 9........1
+		
+		int i=10;
+		do 
+		{
+		System.out.println(i);
+		i--;					
+		}while(i>=1);
+		
+	}
+
+}
