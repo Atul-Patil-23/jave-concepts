ee8dcf8dda468d60672b282ab1d94381b233372c
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:16:53 +0530

    Create ReverseAnArray.java

---
+++ Day7/ReverseAnArray.java
@@ -0,0 +1,16 @@
+package day7;
+
+import java.util.Arrays;
+
+public class ReverseAnArray {
+
+	public static void main(String[] args) {
+		int a[]= {3,4,6,2,5,1,9};
+		for(int i=a.length-1; i>=0; i--)
+		{
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
