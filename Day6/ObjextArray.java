eb5ed83fbf27143c1511eacca989fa14f2d8ad6a
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:07:14 +0530

    Create ObjextArray.java

---
+++ Day6/ObjextArray.java
@@ -0,0 +1,15 @@
+package day6;
+
+public class ObjextArray {
+
+	public static void main(String[] args) {
+		
+		// 
+		Object a[]= {11,12.3,'A',"String"};
+		for(Object x:a) // for each loop
+		{
+			System.out.println(x);
+		}
+	}
+
+}
