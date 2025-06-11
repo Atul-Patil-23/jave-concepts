9dad73e2ee9174ec772fa1a6e83234dcc54e3e98
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:04:36 +0530

    Create Whileloop.java

---
+++ Day5/Whileloop.java
@@ -0,0 +1,50 @@
+package day5;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		/*int i=1;  //initialization
+		while(i<=10)   // condition
+		{
+			System.out.println(i);
+			i++;
+		}
+*/
+		
+		
+		//Print even and odd number
+		
+		/*int i=1;     //initialization
+		while(i<=10)      // condition
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+" Even number");
+			}
+			else 
+			{
+				System.out.println(i+" Odd number");
+			}
+			
+			i++;
+			
+		}
+		*/
+		
+		
+	//print descending order
+		
+		int i=10;           // initialization
+		while(i>=1)       // condition
+		{ 
+			System.out.println(i);
+			i--;
+			
+		}
+		
+		
+		
+		
+	}
+
+}
