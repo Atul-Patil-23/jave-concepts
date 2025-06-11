d614ccfea0bb653d2f66abe6036467ba99b94cfd
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:18:39 +0530

    Create SumOfArray.java

---
+++ Day7/SumOfArray.java
@@ -0,0 +1,15 @@
+package day7;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,30,50};
+		int sum=0;
+		for(int value:a)
+		{
+			sum=sum+value;
+		}
+		System.out.println(sum);
+	}
+
+}
