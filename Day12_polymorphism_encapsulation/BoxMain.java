45466341047cf9627df3219405f0cfac10f04bbf
Atul-Patil-23 <atul23395patil@gmail.com>
2025-06-11 17:32:33 +0530

    Create BoxMain.java

---
+++ Day12_polymorphism_encapsulation/BoxMain.java
@@ -0,0 +1,16 @@
+package day12_polymorphism_encapsulation;
+
+public class BoxMain {
+
+	public static void main(String[] args) {
+		//Box_constructor_overloading bx=new Box_constructor_overloading();  //1
+		//Box_constructor_overloading bx=new Box_constructor_overloading(11.1,12.2,13.3); //2
+		Box_constructor_overloading bx=new Box_constructor_overloading(12.7);
+		
+		
+		
+		bx.volumn();
+
+	}
+
+}
